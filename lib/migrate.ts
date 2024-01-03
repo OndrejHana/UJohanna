483c50dd67a4f56c870a58e33830407f12bb0197
OndrejHana <ondrahana001@gmail.com>
2024-01-03 18:19:42 +0100

    removed migrations

--- lib/migrate.ts
+++
@@ -1,4 +0,0 @@
-import { migrate } from 'drizzle-orm/planetscale-serverless/migrator';
-import { db, connection } from './db';
-
-await migrate(db, { migrationsFolder: '@/migrations' });
--- migrations/0002_puzzling_prowler.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `objednavky` MODIFY COLUMN `id` int AUTO_INCREMENT NOT NULL;--> statement-breakpoint
-ALTER TABLE `produkty` MODIFY COLUMN `id` int AUTO_INCREMENT NOT NULL;--> statement-breakpoint
-ALTER TABLE `objednavky` ADD `datum` timestamp DEFAULT (now());--> statement-breakpoint
-ALTER TABLE `objednavky` ADD `produktId` int NOT NULL;--> statement-breakpoint
-CREATE INDEX `cenaIdx` ON `produkty` (`cena`);--> statement-breakpoint
-ALTER TABLE `objednavky` ADD CONSTRAINT `objednavky_produktId_produkty_id_fk` FOREIGN KEY (`produktId`) REFERENCES `produkty`(`id`) ON DELETE no action ON UPDATE no action;
